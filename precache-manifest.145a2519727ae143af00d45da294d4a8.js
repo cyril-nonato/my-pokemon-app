a0a539cbc49343a016575f7a1df9fb9782b386ff
Cyril Jessie Nonato <03cyril.nonato@gmail.com>
2019-09-07 16:10:21 +0800

    Updates

---
+++ precache-manifest.145a2519727ae143af00d45da294d4a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9ac740dc0f71be80aac730f3aaee67",
+    "url": "/my-pokemon-app/index.html"
+  },
+  {
+    "revision": "96e64ef184b4f92dd3db",
+    "url": "/my-pokemon-app/static/css/main.b8509884.chunk.css"
+  },
+  {
+    "revision": "074ea434e3ce6331ffee",
+    "url": "/my-pokemon-app/static/js/2.69da7e08.chunk.js"
+  },
+  {
+    "revision": "d73ff39cf5b79728459c",
+    "url": "/my-pokemon-app/static/js/3.2a6ad8c6.chunk.js"
+  },
+  {
+    "revision": "eafe92ecb29d02722e1f",
+    "url": "/my-pokemon-app/static/js/4.5e53ef13.chunk.js"
+  },
+  {
+    "revision": "96e64ef184b4f92dd3db",
+    "url": "/my-pokemon-app/static/js/main.20b7edcf.chunk.js"
+  },
+  {
+    "revision": "78a38b7f5e2d5655908f",
+    "url": "/my-pokemon-app/static/js/runtime~main.43d2b072.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-pokemon-app/precache-manifest.2cf0eb94a9152a869449f251ad177bc8.js"
+  "/my-pokemon-app/precache-manifest.145a2519727ae143af00d45da294d4a8.js"
 );
 
 self.addEventListener('message', (event) => {
