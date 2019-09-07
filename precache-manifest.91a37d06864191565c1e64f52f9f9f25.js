574c3250d47e39da9a2811876bfc5d670cbb8529
Cyril Jessie Nonato <03cyril.nonato@gmail.com>
2019-09-07 15:29:20 +0800

    Updates

---
+++ precache-manifest.91a37d06864191565c1e64f52f9f9f25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c08428a4fa5f9b25bf775fa01f4eaf",
+    "url": "/my-pokemon-app/index.html"
+  },
+  {
+    "revision": "41ee032155e3aa56223c",
+    "url": "/my-pokemon-app/static/css/main.b5e357f3.chunk.css"
+  },
+  {
+    "revision": "ea3099cfcf915868bb8d",
+    "url": "/my-pokemon-app/static/js/2.d35368d0.chunk.js"
+  },
+  {
+    "revision": "a95d8bdbb50fe6c04f5e",
+    "url": "/my-pokemon-app/static/js/3.ccd26b3b.chunk.js"
+  },
+  {
+    "revision": "aa0cea9b88be25eac0b0",
+    "url": "/my-pokemon-app/static/js/4.11d10f0b.chunk.js"
+  },
+  {
+    "revision": "41ee032155e3aa56223c",
+    "url": "/my-pokemon-app/static/js/main.879f8c7c.chunk.js"
+  },
+  {
+    "revision": "53fd141a4fc5bbe4347e",
+    "url": "/my-pokemon-app/static/js/runtime~main.025474ec.js"
+  }
+]);
